194efbbd21336cdf18151f06a5fbc1db47ead3bd
Martin Moreno Jara <martin.moreno.j04@gmail.com>
2023-02-19 22:56:35 -0500

    función popback retorna el valor eliminado

--- src/StaticArrayList.java
+++ src/StaticArrayList.java
@@ -2,6 +2,7 @@ public class StaticArrayList {
     int capacity;
     int[] list;
     int size;
+    //constructors------------------------------------
     public StaticArrayList(int cap){
         this.capacity = cap;
         this.size = 0;
@@ -11,8 +12,12 @@ public class StaticArrayList {
         this(5);
         this.size = 0;
     }
+    //-------------------------------------------------
     public boolean isFull(){
         return size==capacity;
+    }
+    public boolean isEmpty(){
+        return size==0;
     }
     public void pushBack(int value){
         if(!isFull()){
@@ -22,12 +27,11 @@ public class StaticArrayList {
             System.out.println("List is full");
         }
     }
-    public boolean isEmpty(){
-        return size==0;
-    }
     public void popBack(){
+        int aux =size;
         if(!isEmpty()){
             size--;
+            System.out.println("poped value: "+aux);
         }
         else{
             System.out.println("List is empty");
